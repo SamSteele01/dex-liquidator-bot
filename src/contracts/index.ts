60941b4e20dd6b692dcb3aa5c8782f2c0f1c7ce4
SamSteele01 <ssteele017@gmail.com>
2020-11-04 18:05:05 -0500

    export contracts, comment out for testing

---
+++ src/contracts/index.ts
@@ -0,0 +1,11 @@
+import { default as aave } from './aave';
+// import { default as bzx } from './bzx';
+// import { default as compound } from './compound';
+// import { default as dydx } from './dydx';
+
+export const contracts = {
+  aave,
+  // bzx,
+  // compound,
+  // dydx,
+};
