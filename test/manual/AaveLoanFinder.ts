67ad2f289c01ff26f36e9330aaed83134a8b61dd
SamSteele01 <ssteele017@gmail.com>
2020-11-04 18:26:34 -0500

    add test file for loanFinder/Aave.ts

---
+++ test/manual/AaveLoanFinder.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import Web3 from 'web3';
+import Aave from '../../src/loanFinder/Aave';
+
+dotenv.config();
+
+async function main() {
+  const web3 = new Web3(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`);
+
+  const aave = new Aave(web3);
+  await aave.requestAndSetAllBorrowEvents();
+  // await aave.getAllLiquidationEvents('0x53F0F70d1b8EF10FE8267C9D79ef77487C2D2bd6');
+  // await aave.getAllDepositEvents('0x53F0F70d1b8EF10FE8267C9D79ef77487C2D2bd6');
+  await aave.getAllUsersAccountData();
+  aave.printEligibleLoans();
+}
+
+main();
