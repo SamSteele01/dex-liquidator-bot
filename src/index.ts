db90d7c71e1897434b98e37fc42961b7901297ef
Sam Steele <ssteele017@gmail.com>
2020-12-02 13:51:36 -0500

    add rate limiter

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import Web3 from 'web3';
 import { createConnection } from 'typeorm';
 import { exchanges, tokens } from './config';
 import ListenToRelevantTokenPrices from './ListenToRelevantTokenPrices';
+import { RateLimiter } from 'limiter';
 import AaveBacklog from './lendingExchanges/aave/Backlog';
 import GasPriceWatcher from './GasPriceWatcher';
 
@@ -14,16 +15,17 @@ async function main() {
   // initialize - connections to Web3, Redis and Postgres?
   const web3ws = new Web3('ws://localhost:8546'); // Ganache
   const dbConnection = await createConnection();
-  // RateLimiter
   // ErrorHandler
   // MempoolWatcher
   const commonEmitter = new EventEmitter() as TypedEmitter<MessageEvents>;
+  const rateLimiter = new RateLimiter(50, 'second');
 
   const gasPriceWatcher = new GasPriceWatcher();
   gasPriceWatcher.start();
 
   /* start loan finders, get all existing loans from exchanges that we are watching */
   const aaveLoans = new Aave(web3ws);
+  const aaveBacklog = new AaveBacklog(web3ws, rateLimiter, aaveLendingPoolAddress);
 
   /* get caught up on backlog */
   const backlogs: Promise<any>[] = [];
